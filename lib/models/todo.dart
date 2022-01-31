46bf4bb55eaf448336520395e3f0c2fa6c4611d2
Aditya Deshmukh <8530945813aditya@gmail.com>
2022-01-31 18:16:38 +0530

    initial commit

---
+++ lib/models/todo.dart
@@ -0,0 +1,23 @@
+class Todo {
+  late int? id ;
+ 
+  late String description;
+
+  bool isDone = false;
+
+  Todo({ this.id ,required this.description, this.isDone = false});
+
+  factory Todo.fromDatabaseJson(Map<String, dynamic> data) => Todo(
+
+        id: data['id'],
+        description: data['description'],
+        isDone: data['is_done'] == 0 ? false : true,
+
+      );
+
+  Map<String, dynamic> toDatabaseJson() => {
+        "id": id,
+        "description": description,
+        "is_done": isDone == false ? 0 : 1,
+      };
+}
